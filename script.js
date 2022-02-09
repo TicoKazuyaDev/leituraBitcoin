1d8d4d536da0f97cea379a84d79d65ca1593c4fb
TicoKazuyaDev <tcosta847@gmail.com>
2022-02-09 19:54:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var nome = "Tiago";
+/*var nome = "Tiago";
 
 console.log(nome);
 
 var variavel;
 variavel = prompt("Informe um valor para essa variável.");
-alert(variavel);
+alert(variavel);*/
+
+/*Maçãns 
+var numeroMacas = 4;
+var todasAsMacasVerdes = false;
+
+if(numeroMacas > 1 && todasAsMacasVerdes === true){
+    console.log("Temos mais de uma maçã e todassão verdes.");
+}
+else{
+    console.log("Ou não temos mais de uma maçã, ou nem todas são verdes.");
+}*/
+
