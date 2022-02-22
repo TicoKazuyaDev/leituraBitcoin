cd8bc0393d008499efc6ee2ba07eeddf521bc62a
TicoKazuyaDev <tcosta847@gmail.com>
2022-02-22 11:20:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 Iniciando o JavaScript
 Tiago da Costa Jerônimo
 */
+
+var Carro = function(nome, car, marca, ano){
+    this.nome = nome;
+    this.cor = cor;
+    this.marca = marca;
+    this.ano = ano;
+};
+
+var fusca = new Carro('Fusca', 'azul','VW', '1975' );
+
+console.log(fusca);
